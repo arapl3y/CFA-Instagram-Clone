1b8fb26e5d52c9ac76097bef5c1773f7f2d8d3a4
Alex Rapley <rapley3@gmail.com>
2017-03-14 17:36:29 +1100

    Added edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post = Post.update(post_params)
+    redirect_to(posts_path(@path))
+  end
+
   private
 
   def post_params
